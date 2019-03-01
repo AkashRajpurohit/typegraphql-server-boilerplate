e548170bfabc86e308caf428b174027326fca08a
Akash Rajpurohit <akash009.dev@gmail.com>
2019-03-01 21:18:26 +0530

    Register Resolver working

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,15 @@
 import { ApolloServer } from 'apollo-server-express'
 import * as Express from 'express'
 import 'reflect-metadata'
-import { buildSchema, Query, Resolver } from 'type-graphql'
+import { buildSchema } from 'type-graphql'
 import { createConnection } from 'typeorm'
-
-@Resolver()
-class HelloResolver {
-  @Query(() => String, { nullable: true })
-  async hello() {
-    return 'Hello World'
-  }
-}
+import { RegisterResolver } from './modules/user/Register'
 
 const main = async () => {
   await createConnection()
 
   const schema = await buildSchema({
-    resolvers: [HelloResolver],
+    resolvers: [RegisterResolver],
   })
 
   const apolloServer = new ApolloServer({ schema })
